a4301417eaa1fdd99515179a1982b1e62a2ef599
disencd@gmail.com <ddevassy>
2021-11-09 22:36:55 -0800

    Create security group + vpc

--- prod.tf
+++ prod.tf
@@ -1,13 +1,40 @@
 provider "aws" {
   profile = "terraform"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_s3_bucket" "prod_tf_s3" {
   bucket = "s3-disencd-prod"
-  acl = "private"
+  acl    = "private"
 }
 
 resource "aws_default_vpc" "default" {
+}
 
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http & https ports inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
 }
