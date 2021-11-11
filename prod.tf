68ccdc4c16affd8363d4d6f66f5fb5f1eb40bc1f
disencd@gmail.com <ddevassy>
2021-11-11 13:31:13 -0800

    Created nginx server

--- prod.tf
+++ prod.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   profile = "terraform"
+//  profile = "default"
   region  = "us-west-2"
 }
 
 resource "aws_s3_bucket" "prod_tf_s3" {
-  bucket = "s3-disencd-prod"
+  bucket = "s3-disencd-terraform-test1"
   acl    = "private"
 }
 
@@ -33,6 +34,19 @@ resource "aws_security_group" "prod_web" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_instance" "prod_web_ec2" {
+  ami           = "ami-0d60bcb3cd15a7c5b"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
 
   tags = {
     "Terraform" : "true"
